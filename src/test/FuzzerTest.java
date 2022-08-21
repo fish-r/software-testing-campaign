9c0a2c18ebed2d75e8fb33b90fbf93e19d344eda
Joseph <josephlailailai247@gmail.com>
2022-08-21 23:48:31 +0800

    joseph: check some errors

--- src/test/FuzzerTest.java
+++ src/test/FuzzerTest.java
@@ -1,11 +1,14 @@
 package test;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
 
 public class FuzzerTest {
     private final String input = "fuzz string test input";
+    private final String input1 = "";
+    private final String input2 = "?";
 
     private Fuzzer fuzzer = new Fuzzer();
 
@@ -17,6 +20,13 @@ public class FuzzerTest {
     public void flipBitStringShouldMutateString() {
         String output = fuzzer.flipBitString(input);
         assertNotEquals(input, output);
+    }
+
+    // account for empty string
+    @Test
+    public void flipBitStringShouldMutateString_Joseph() {
+        String output = fuzzer.flipBitString(input1);
+        assertNotEquals(input1, output);
     }
 
     @Test
@@ -35,6 +45,16 @@ public class FuzzerTest {
     public void trimStringShouldTruncateString() {
         String output = fuzzer.trimString(input);
         assertNotEquals(input, output);
+    }
+
+    @Test
+    public void trimStringShouldTruncateString_Joseph() {
+        String output = fuzzer.trimString(input2);
+        // truncate should be to make it shorter
+        System.out.println(output);
+        System.out.println(input2);
+        assertTrue(input2.length() > (output.length()));
+        //assertNotEquals(input2, output);
     }
 
     @Test
